02d9c62ac9a4527270bf965ee4897111aa2e6693
The Weed Doctor <adrianjam123@gmail.com>
2024-02-18 18:11:54 -0400

    rollback try 01

---
+++ src/data/data.js
@@ -0,0 +1,364 @@
+export const data = [
+  // Testo Plus
+  {
+    id: 1,
+    imagebg: "/images/imagebgtest.png",
+    title: "testo-plus",
+    price: 210,
+    transitionname: "ttesto",
+    bgstart: "from-[#526C63]",
+    bgend: "to-[#000]",
+    textcolor: "from-[#294437]",
+    detailstitle: "Testo Plus",
+    description: "Elevador de testosterona natural",
+    pilsData: {
+      principal: "/images/testoplus/testo_principal.png",
+      pils1: "/images/pildora1.png",
+      pils2: "/images/pildora2.png",
+      transitionname: "testo",
+      transitionpils1: "testopils1",
+      transitionpils2: "testopils2",
+    },
+    addTextData: {
+      fontsize: "text-sm",
+      title:
+        "Suplemento Natural y diseñado para estimular al propio cuerpo que genere más testosterona",
+      buttonbg1: "#C0DDC8",
+      buttonbg2: "#2B9D6D",
+      buttontc1: "black",
+      buttontc2: "white",
+      buttongo1: `products/testo-plus`,
+      buttongo2: "buy/testo-plus",
+      buttontitle1: "conoce más",
+      buttontitle2: "Compra ya",
+    },
+    whatis:
+      "Suplemento natural y diseñado para estimular al propio cuerpo que genere más testosterona",
+    howworks:
+      "Su principal uso es aumentar los niveles de testosterona de forma natural en el cuerpo para tener un aumento de fuerza.",
+    ingredients: {
+      ingredient1: "Huanarpo Machi",
+      ingredient2: "Barojo",
+      ingredient3: "Maca Nagra",
+      ingredient4: "Marapuama",
+    },
+    comparation: {
+      natural1: "sin contradicciones",
+      natural2: "saludable y beneficioso para la salud",
+      natural3: "Practico y seguro",
+      artificial1: "Efecto secundarios negativos",
+      artificial2: "Peligroso para la salud",
+      arificial3: "Poco seguro y procedencia dudosa",
+    },
+    howuse:
+      "Levantarse con el mejor de los animos, Estando en ayunas, tomar 2 capsulas de Testo Plus al dia",
+  },
+  // Cartilago de tiburon
+  {
+    id: 2,
+    imagebg: "/images/Group 3.png",
+    title: "cartilago",
+    price: 120,
+    transitionname: "tcartilago",
+    bgstart: "from-[#185B69]",
+    bgend: "to-[#000]",
+    textcolor: "from-[#151A2B]",
+    detailstitle: "Cartilago de tiburon",
+    description: "elevador de testosterona narutal cartilago de tiburon",
+    pilsData: {
+      principal: "/images/cartilago/tiburon_principal.png",
+      pils1: "/images/pildora1.png",
+      pils2: "/images/pildora2.png",
+      transitionname: "cartilago",
+      transitionpils1: "cartilagopils1",
+      transitionpils2: "cartilagopils2",
+    },
+    addTextData: {
+      fontsize: "text-sm",
+      title:
+        "Tejido que proporciona soporte a articulaciones y ligamentos en el cuerpo.",
+      buttonbg1: "#fff",
+      buttonbg2: "#00E0FF",
+      buttontc1: "black",
+      buttontc2: "white",
+      buttongo1: "products/cartilago",
+      buttongo2: "buy/cartilago",
+      buttontitle1: "conoce más",
+      buttontitle2: "compra ya",
+    },
+    whatis:
+      "Tejido que proporciona soporte a articulaciones y ligamentos en el cuerpo",
+    howworks:
+      "Fortalece las articulaciones, previene lesiones, incrementa la regeneracion de cartilagos en el cuerpo",
+    ingredients: {
+      ingredient1: "Cartilago de Tiburon",
+    },
+    comparation: {
+      natural1: "sin contradicciones",
+      natural2: "saludable y beneficioso para la salud",
+      natural3: "Practico y seguro",
+      artificial1: "Efecto secundarios negativos",
+      artificial2: "Peligroso para la salud",
+      arificial3: "Poco seguro y procedencia dudosa",
+    },
+    howuse:
+      "Levantarse con el mejor de los animos, Estando en ayunas, tomar 2 capsulas de Cartilago al dia",
+  },
+  // Maca Negra
+  {
+    id: 3,
+    imagebg: "/images/Group 3.png",
+    title: "maca-negra",
+    price: 100,
+    transitionname: "tmacanegra",
+    bgstart: "from-[#7A6121]",
+    bgend: "to-[#000]",
+    textcolor: "from-[#2B1915]",
+    detailstitle: "Maca Negra",
+    description: "Regulador hormonal y energizante natural.",
+    pilsData: {
+      principal: "/images/macanegra/maca_principal.png",
+      pils1: "/images/pildora1.png",
+      pils2: "/images/pildora2.png",
+      transitionname: "macanegra",
+      transitionpils1: "macapils1",
+      transitionpils2: "macapils2",
+    },
+    addTextData: {
+      fontsize: "text-sm",
+      title:
+        "Regulador hormonal y energizante natural.",
+      buttonbg1: "#C0DDC8",
+      buttonbg2: "#B99B4F",
+      buttontc1: "black",
+      buttontc2: "white",
+      buttongo1: "products/maca-negra",
+      buttongo2: "buy/maca-negra",
+      buttontitle1: "conoce más",
+      buttontitle2: "compra ya",
+    },
+    whatis:
+      "Es un Tuberculo el cual es un regulador hormonal y energizante natural",
+    howworks:
+      "Un potente regulador hormonal y energizante natural. Extraída de los Andes, este superalimento mejora la vitalidad y promueve un bienestar general.",
+    ingredients: {
+      ingredient1: "Cartilago de Tiburon",
+    },
+    comparation: {
+      natural1: "sin contradicciones",
+      natural2: "saludable y beneficioso para la salud",
+      natural3: "Practico y seguro",
+      artificial1: "Efecto secundarios negativos",
+      artificial2: "Peligroso para la salud",
+      arificial3: "Poco seguro y procedencia dudosa",
+    },
+    howuse:
+      "Levantarse con el mejor de los animos, ingesta diaria de 2 capsulas después de alguna comida fuerte.",
+  },
+  // Psyllium
+  {
+    id: 4,
+    imagebg: "/images/imagebgtest.png",
+    title: "psyllium",
+    price: 250,
+    transitionname: "ppsyllium",
+    bgstart: "from-[#294424]",
+    bgend: "to-[#000]",
+    textcolor: "from-[#fff]",
+    detailstitle: "Psyllium",
+    description: "Laxante 100% natural vitaminico",
+    pilsData: {
+      principal: "/images/psyllium/psy_principal.png",
+      pils1: "/images/pildora1.png",
+      pils2: "/images/pildora2.png",
+      transitionname: "psyllium",
+      transitionpils1: "psyllium1",
+      transitionpils2: "psyllium2",
+    },
+    addTextData: {
+      fontsize: "text-sm",
+      title:
+        "Laxante 100% natural el cual aporta fibra en gran cantidad y multiples vitaminas al cuerpo.",
+      buttonbg1: "#C0DDC8",
+      buttonbg2: "#2B9D6D",
+      buttontc1: "black",
+      buttontc2: "white",
+      buttongo1: "products/psyllium",
+      buttongo2: "buy/psyllium",
+      buttontitle1: "conoce más",
+      buttontitle2: "compra ya",
+    },
+    whatis:
+      "Es un Tuberculo el cual es un regulador hormonal y energizante natural",
+    howworks:
+      "Un remedio 100% natural, rico en fibra y vitaminas. Su alto contenido de fibra es ideal para combatir el estreñimiento, a la vez que previene enfermedades cardiacas y controla la diabetes.",
+    ingredients: {
+      ingredient1: "Psyllium",
+    },
+    comparation: {
+      natural1: "sin contradicciones",
+      natural2: "saludable y beneficioso para la salud",
+      natural3: "Practico y seguro",
+      artificial1: "Efecto secundarios negativos",
+      artificial2: "Peligroso para la salud",
+      arificial3: "Poco seguro y procedencia dudosa",
+    },
+    howuse:
+      "Se toman 2 capsulas de preferencia con alguna comida, ingerir la cantidad adecuada de agua.",
+  },
+  // Combo 1
+  {
+    id: 5,
+    imagebg: "/images/imagebgtest.png",
+    title: "combo1",
+    price: 250,
+    transitionname: "ccombotyc1",
+    bgstart: "from-[#126C23]",
+    bgend: "to-[#185B69]",
+    textcolor: "from-[#fff]",
+    detailstitle: "Testo Plus y Cartilago",
+    description:
+      "Compuesto por una combinacion de Testo Plus y Cartilago de Tiburon",
+    pilsData: {
+      principal: "/images/combo1/tyc.png",
+      pils1: "/images/pildora1.png",
+      pils2: "/images/pildora2.png",
+      transitionname: "combotyc",
+      transitionpils1: "combotyc1",
+      transitionpils2: "combotyc2",
+    },
+    addTextData: {
+      fontsize: "text-sm",
+      title:
+        "Suplemento Natural y diseñado para estimular al propio cuerpo que genere más testosterona",
+      buttonbg1: "#C0DDC8",
+      buttonbg2: "#2B9D6D",
+      buttontc1: "black",
+      buttontc2: "white",
+      buttongo1: "products/combo1",
+      buttongo2: "buy/combo1",
+      buttontitle1: "conoce más",
+      buttontitle2: "compra ya",
+    },
+    whatis:
+      "Es un Tuberculo el cual es un regulador hormonal y energizante natural",
+    howworks:
+      "Su principal funcion es mantener un balance en tu sistema hormonal y aportar energia extra para tus actividades diarias",
+    ingredients: {
+      ingredient1: "Psyllium",
+    },
+    comparation: {
+      natural1: "sin contradicciones",
+      natural2: "saludable y beneficioso para la salud",
+      natural3: "Practico y seguro",
+      artificial1: "Efecto secundarios negativos",
+      artificial2: "Peligroso para la salud",
+      arificial3: "Poco seguro y procedencia dudosa",
+    },
+    howuse:
+      "Levantarse con el mejor de los animos, Estando en ayunas, tomar 2 capsulas de Testo Plus al dia",
+  },
+  // Combo 2
+  {
+    id: 6,
+    imagebg: "/images/imagebgtest.png",
+    title: "combo2",
+    price: 260,
+    transitionname: "ccombotym1",
+    bgstart: "from-[#526C63]",
+    bgend: "to-[#7A6121]",
+    textcolor: "from-[#fff]",
+    detailstitle: "testo plus y maca negra",
+    description:
+      "Compuesto por una combinacion de Testo Plus y Cartilago de Tiburon",
+    pilsData: {
+      principal: "/images/combo2/tym.png",
+      pils1: "/images/pildora1.png",
+      pils2: "/images/pildora2.png",
+      transitionname: "combotym",
+      transitionpils1: "combotym1",
+      transitionpils2: "combotym2",
+    },
+    addTextData: {
+      fontsize: "text-sm",
+      title:
+        "Suplemento Natural y diseñado para estimular al propio cuerpo que genere más testosterona",
+      buttonbg1: "#C0DDC8",
+      buttonbg2: "#2B9D6D",
+      buttontc1: "black",
+      buttontc2: "white",
+      buttongo1: "products/combo2",
+      buttongo2: "buy/combo2",
+      buttontitle1: "conoce más",
+      buttontitle2: "compra ya",
+    },
+    whatis:
+      "Es un Tuberculo el cual es un regulador hormonal y energizante natural",
+    howworks:
+      "Su principal funcion es mantener un balance en tu sistema hormonal y aportar energia extra para tus actividades diarias",
+    ingredients: {
+      ingredient1: "Psyllium",
+    },
+    comparation: {
+      natural1: "sin contradicciones",
+      natural2: "saludable y beneficioso para la salud",
+      natural3: "Practico y seguro",
+      artificial1: "Efecto secundarios negativos",
+      artificial2: "Peligroso para la salud",
+      arificial3: "Poco seguro y procedencia dudosa",
+    },
+    howuse:
+      "Levantarse con el mejor de los animos, Estando en ayunas, tomar 2 capsulas de Testo Plus al dia",
+  },
+  // Combo 3
+  {
+    id: 7,
+    imagebg: "/images/imagebgtest.png",
+    title: "combo3",
+    price: 310,
+    transitionname: "ccomboteu1",
+    bgstart: "from-[#526C63]",
+    bgend: "to-[#7A6121]",
+    textcolor: "from-[#fff]",
+    detailstitle: "testo plus y maca negra",
+    description:
+      "Compuesto por una combinacion de Testo Plus y Cartilago de Tiburon",
+    pilsData: {
+      principal: "/images/combo3/mtc.png",
+      pils1: "/images/pildora1.png",
+      pils2: "/images/pildora2.png",
+      transitionname: "comboteu",
+      transitionpils1: "comboteu1",
+      transitionpils2: "comboteu2",
+    },
+    addTextData: {
+      fontsize: "text-sm",
+      title: "Compra los 3 suplementos con un increible descuento!",
+      buttonbg1: "#C0DDC8",
+      buttonbg2: "#2B9D6D",
+      buttontc1: "black",
+      buttontc2: "white",
+      buttongo1: "products/combo3",
+      buttongo2: "buy/combo3",
+      buttontitle1: "conoce más",
+      buttontitle2: "compra ya",
+    },
+    whatis:
+      "Un super combo de suplementos naturales para mejorar tu salud y rendimiento fisico",
+    howworks:
+      "Su principal funcion es mantener un balance en tu sistema hormonal y aportar energia extra para tus actividades diarias",
+    ingredients: {
+      ingredient1: "Psyllium",
+    },
+    comparation: {
+      natural1: "sin contradicciones",
+      natural2: "saludable y beneficioso para la salud",
+      natural3: "Practico y seguro",
+      artificial1: "Efecto secundarios negativos",
+      artificial2: "Peligroso para la salud",
+      arificial3: "Poco seguro y procedencia dudosa",
+    },
+    howuse:
+      "Levantarse con el mejor de los animos, Estando en ayunas, tomar 2 capsulas de Testo Plus al dia",
+  },
+];
