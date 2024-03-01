1813ad8eee5af54870c23c547ba6453874160407
Horacio Cortez <leohermoso18@gmail.com>
2024-03-01 19:53:58 -0300

    push
    push

--- app/src/pages/AdminPage.jsx
+++ app/src/pages/AdminPage.jsx
@@ -5,13 +5,14 @@ export default function AdminPage() {
   const [error, setError] = useState(null);
   const [voucher, setVoucher] = useState(null);
 
-  useEffect(() => {
+  const init = () => {
     CheckAdmin();
+    GetVouchers();
+  };
 
-    setTimeout(() => {
-      GetVouchers();
-    }, 1000);
-  });
+  useEffect(() => {
+    init();
+  }, []);
 
   function CheckAdmin() {
     fetch("http://localhost:8080/validateadmin", {
@@ -50,12 +51,12 @@ export default function AdminPage() {
   }
 
   return (
-    <div className="bg-gradient-to-br from-[#185B69] to-black h-screen w-screen flex text-white p-20">
+    <div className="bg-gradient-to-br from-[#185B69] to-black h-full min-h-screen w-screen flex md:flex-row flex-col text-white p-20">
       {voucher && (
-        <div className="absolute h-screen w-screen bg-black/80 top-0 left-0 z-40">
+        <div className=" h-screen w-screen bg-black/80 top-0 left-0 z-40 fixed">
           <div className="h-full w-full flex flex-col items-center justify-center">
             <h1 className="text-4xl font-bold">Admin</h1>
-            <div>
+            <div className=" border border-white p-4 rounded-lg">
               <h1>Nombre: {voucher.Users.username}</h1>
               <h1>Email: {voucher.Users.email}</h1>
               <h1>Telefono: {voucher.Users.phone}</h1>
@@ -63,7 +64,13 @@ export default function AdminPage() {
               <h1>
                 Entrega: {voucher.delivery ? voucher.Users.address : "no"}
               </h1>
-              <h1>Estado: {voucher.status ? "confirmado" : "pendiante"}</h1>
+              <h1
+                className={` ${
+                  voucher.status ? `bg-green-500` : `bg-blue-500`
+                } text-white rounded-lg p-2 w-max`}
+              >
+                Estado: {voucher.status ? "confirmado" : "pendiante"}
+              </h1>
 
               <img
                 className="h-80 w-auto"
@@ -71,7 +78,7 @@ export default function AdminPage() {
               />
             </div>
 
-            <div>
+            <div className="mt-4">
               <button
                 className="bg-green-500 text-white rounded-lg p-2"
                 onClick={() => {
@@ -114,7 +121,12 @@ export default function AdminPage() {
                 Eliminar
               </button>
             </div>
-            <button onClick={() => setVoucher(null)}>Cancelar</button>
+            <button
+              className="mt-5 bg-transparent border border-white px-4 py-2 rounded-xl text-white"
+              onClick={() => setVoucher(null)}
+            >
+              Cancelar
+            </button>
           </div>
         </div>
       )}
@@ -126,18 +138,18 @@ export default function AdminPage() {
             Todos los Vouchers
           </li>
           {/* <li className="hover:scale-105 select-none hover:bg-blue-500 hover:text-white hover:border hover:border-white transition-colors ease-in-out duration-150 delay-75 rounded-lg px-2 py-1">
-            Pendientes
-          </li>
-          <li className="hover:scale-105 selec hover:bg-blue-500 hover:text-white hover:border hover:border-white transition-colors ease-in-out duration-150 delay-75 rounded-lg px-2 py-1">
+            Eliminados
+          </li> */}
+          {/* <li className="hover:scale-105 selec hover:bg-blue-500 hover:text-white hover:border hover:border-white transition-colors ease-in-out duration-150 delay-75 rounded-lg px-2 py-1">
             Aceptados
           </li> */}
         </ul>
       </aside>
-      <section>
+      <section className="md:block hidden">
         <h1>Todos los vouchers</h1>
-        <table>
+        <table className="">
           <thead>
-            <tr>
+            <tr className="[&>th]:border [&>th]:px-2 [&>th]:border-white">
               <th>Nombre</th>
               <th>Correo</th>
               <th>Telefono</th>
@@ -159,7 +171,15 @@ export default function AdminPage() {
                   {new Date(voucher.CreatedAt).toLocaleDateString()}{" "}
                   {new Date(voucher.CreatedAt).toLocaleTimeString()}
                 </td>
-                <td>{voucher.status ? "Confirmado" : "Pendiente"}</td>
+                <td
+                  className={` ${
+                    voucher.status ? `bg-green-500` : `bg-blue-500`
+                  } text-white p-2 w-max
+                  
+                  `}
+                >
+                  {voucher.status ? "Confirmado" : "Pendiente"}
+                </td>
                 <td>
                   <img
                     className="h-10 w-auto "
@@ -181,6 +201,38 @@ export default function AdminPage() {
             ))}
           </tbody>
         </table>
+      </section>
+      <section className="md:hidden block">
+        <h1>Todos los vouchers</h1>
+        {vouchers.map((voucher) => (
+          <div
+            key={voucher.ID}
+            className="p-2 border border-white rounded-lg my-2 flex flex-col"
+          >
+            <h1>Nombre: {voucher.Users.username}</h1>
+            <h1>Email: {voucher.Users.email}</h1>
+            <h1>Telefono: {voucher.Users.phone}</h1>
+            <h1>
+              Fecha: {new Date(voucher.CreatedAt).toLocaleDateString()}{" "}
+              {new Date(voucher.CreatedAt).toLocaleTimeString()}
+            </h1>
+            <h1>Estado: {voucher.status ? "Confirmado" : "Pendiente"}</h1>
+            <img
+              className="h-auto w-auto "
+              src={`http://localhost:8080/voucher/images/${voucher.ID}`}
+            />
+            <h1>Entrega: {voucher.delivery ? voucher.Users.address : "no"}</h1>
+            <h1>Metodo de pago: {voucher.payment_method}</h1>
+            <button
+              onClick={() => {
+                setVoucher(voucher);
+              }}
+              className="bg-green-500 text-white rounded-lg p-2"
+            >
+              Ver
+            </button>
+          </div>
+        ))}
       </section>
     </div>
   );
